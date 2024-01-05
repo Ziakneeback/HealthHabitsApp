03f8f88ff49d4274e37dcb1d9251ebacebd3eb8a
Zhanibek Asylbek <jonimoni84@gmail.com>
2024-01-05 16:22:41 +0600

    Initial Commit

---
+++ HealthHabitsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HealthHabitsApp
+//
+//  Created by Жанибек Асылбек on 05.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
